b423f5fb0b4e898e1572904f5d813ab0b6818269
joelsrzalvarez <contactojoelsuarez@gmail.com>
2024-02-21 14:26:53 +0100

    add insert.js, extracr.js #340

---
+++ staff/joel-suarez/objects/insert.js
@@ -0,0 +1,97 @@
+/**
+ * Inserts an element in iterable object at specfified index.
+ *
+ * @param object - The iterable object to mutate. 
+ * @param index - The index from which to insert the given values.
+ * @param value - The value to insert.
+ * 
+ * @throws {TypeError} When object is not an object, or when index is not a number.
+ */
+function insert(object, index, ...values) {
+    if (!object || !(object instanceof Object)) {
+        throw new TypeError('Object parameter is required and must be an Object');
+    }
+    if (typeof index !== 'number') {
+        throw new TypeError('Index parameter is required and must be a number');
+    }
+    var originalLength = object.length;
+    var numValues = values.length;
+
+    // mover los elementos hacia la derecha para hacer espacio para los nuevos valores
+    for (var i = originalLength + numValues - 1; i > index + numValues - 1; i--) {
+        object[i] = object[i - numValues];
+    }
+
+    // insertar los nuevos valores en el objeto en la posicion que le decimos
+    for (var j = 0; j < numValues; j++) {
+        object[index + j] = values[j];
+    }
+
+    // aumentar la propiedad length
+    object.length += numValues;
+}
+console.log('CASE 1: insert skyblue in index 1'); // LOGRADO
+
+var colors = {
+    0: 'red',
+    1: 'blue',
+    2: 'green',
+    length: 3
+}
+
+var length = insert(colors, 1, 'skyblue');
+
+console.log(length)
+// 4
+
+console.log(colors)
+
+console.log('CASE 2: insert skyblue, gold and plum in index 2') // LOGRADO
+
+var colors = {
+    0: 'red',
+    1: 'blue',
+    2: 'green',
+    length: 3
+}
+
+var length = insert(colors, 2, 'skyblue', 'gold', 'plum')
+
+console.log(length)
+// 6
+
+console.log(colors)
+
+console.log('CASE 3: fails on undefind object parameter')  // LOGRADO
+
+try {
+    insert(undefined, 2, 'skyblue', 'gold', 'plum');
+} catch (error) {
+    console.log(error)
+    // TypeError: undefined is not an Object
+}
+
+console.log('CASE 4: fails on 1 as a object parameter') //logrado
+
+try {
+    insert(null, 2, 'skyblue', 'gold', 'plum');
+} catch (error) {
+    console.log(error)
+    // TypeError: 1 is not an Object
+}
+
+console.log('CASE 5: fails on undefined as index parameter') // logrado
+
+var colors = {
+    0: 'red',
+    1: 'blue',
+    2: 'green',
+    length: 3
+}
+
+try {
+    insert(colors)
+} catch (error) {
+    console.log(error)
+    // TypeError: undefined is not a Number
+}
