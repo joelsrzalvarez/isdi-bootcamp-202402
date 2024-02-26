2c6e69447301eae38fec6351be0bb896a404a1c6
joelsrzalvarez <contactojoelsuarez@gmail.com>
2024-02-26 15:20:51 +0100

    changed definition of methods to Car.prototype instead use this. #398

--- staff/joel-suarez/prototypes/car/Car.js
+++ staff/joel-suarez/prototypes/car/Car.js
@@ -14,45 +14,45 @@ function Car(brand, model, year, color, doors, fuelType, transmission, gears) {
     this.acceleration = 0;
     this.direction = '';
     this.steering = 0;
-
-    this.fuel = function (amount) {
-        this.deposit = amount;
-    };
-
-    this.start = function () {
-        this.status = 'on';
-    };
-
-    this.stop = function () {
-        this.status = 'off';
-    };
-
-    this.changeGear = function (gear) {
-        if (gear > this.gears || gear < -1) {
-            throw new RangeError('gear greater than ' + this.gears);
-        }
-        this.gear = gear;
-    };
-
-    this.speedUp = function (acceleration) {
-        this.acceleration = acceleration;
-        if (this.gear === 1) {
-            this.direction = 'forward';
-        }
-        else if (this.gear === -1) {
-            this.direction = 'backward';
-        }
-    };
-
-    this.changeSteering = function (angle) {
-        this.steering = angle;
-        if (this.acceleration > 0 && this.gear === 1) {
-            this.direction = 'forward-right';
-        }
-        else if (this.acceleration > 0 && this.gear === -1) {
-            this.direction = 'backward-left';
-        }
-    };
 }
+
+Car.prototype.fuel = function (amount) {
+    this.deposit = amount;
+};
+
+Car.prototype.start = function () {
+    this.status = 'on';
+};
+
+Car.prototype.stop = function () {
+    this.status = 'off';
+};
+
+Car.prototype.changeGear = function (gear) {
+    if (gear > this.gears || gear < -1) {
+        throw new RangeError('gear greater than ' + this.gears);
+    }
+    this.gear = gear;
+};
+
+Car.prototype.speedUp = function (acceleration) {
+    this.acceleration = acceleration;
+    if (this.gear === 1) {
+        this.direction = 'forward';
+    }
+    else if (this.gear === -1) {
+        this.direction = 'backward';
+    }
+};
+
+Car.prototype.changeSteering = function (angle) {
+    this.steering = angle;
+    if (this.acceleration > 0 && this.gear === 1) {
+        this.direction = 'forward-right';
+    }
+    else if (this.acceleration > 0 && this.gear === -1) {
+        this.direction = 'backward-left';
+    }
+};
 
 module.exports = Car;
